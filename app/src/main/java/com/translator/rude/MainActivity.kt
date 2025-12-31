3357cc65b87bb55cf8cb4b4fa181da3bfb4d8af6
15cmac65-ship-it <15cmac65@gmail.com>
2026-01-01 00:23:09 +0100

    Create MainActivity.kt

---
+++ app/src/main/java/com/translator/rude/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.translator.rude
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val tv = TextView(this).apply {
+            text = "Translator RU ⇄ DE\n(первый запуск)"
+            textSize = 22f
+            setPadding(40, 80, 40, 40)
+        }
+        setContentView(tv)
+    }
+}
